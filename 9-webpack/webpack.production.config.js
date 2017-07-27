d6e06ac3d5f94c16a0a16bf1a31ff2f6f424ef28
Yuanyuan Li <yl2577@email.com>
2017-07-26 21:07:51 -0400

    9 update

--- 9-webpack/webpack.production.config.js
+++ 9-webpack/webpack.production.config.js
@@ -3,33 +3,36 @@ var path = require('path')
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
-module.export = {
+module.exports = {
     devtool: 'eval-source-map',
     entry: {
-        app: path.resolve(__dirname + "app/index.jsx"),
+        app: path.resolve(__dirname + "/app/index.js"),
         vendor: Object.keys(pkg.dependencies)
     },
     output: {
         path: __dirname + "/build",
-        filename: "/js/[name]-[chunkhash:8].js"
+        filename: "js/[name]-[chunkhash:8].js",
+        publicPath: '/'
     },
     resolve:{
-        extensions:['', '.js', '.jsx']
+        extensions:['.js', '.jsx']
     },
     module: {
         loaders: [
-        { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel' },
-        { test: /\.less$/, exclude: /node_modules/, loader: ExtractTextPlugin.extract({fallback:'style', use:'css!postcss!less'}) },
-        { test: /\.css$/, exclude: /node_modules/, loader: ExtractTextPlugin.extract({fallback:'style', use:'css!postcss'}) },
-        { test:/\.(png|gif|jpg|jpeg|bmp)$/i, loader:'url-loader?limit=5000&name=img/[name].[chunkhash:8].[ext]' },
-        { test:/\.(png|woff|woff2|svg|ttf|eot)($|\?)/i, loader:'url-loader?limit=5000&name=fonts/[name].[chunkhash:8].[ext]'}
+        { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel-loader' },
+        { test: /\.less$/, exclude: /node_modules/, loader: ExtractTextPlugin.extract({fallback:'style-loader', use:'css-loader!less-loader'}) },
+        { test: /\.css$/, exclude: /node_modules/, loader: 'style-loader!css-loader?modules' },
+        { test:/\.(png|gif|jpg|jpeg|bmp)$/i, loader:'url-loader?limit=5000&name=img/.[ext]' },
+        { test:/\.(png|woff|woff2|svg|ttf|eot)($|\?)/i, loader:'url-loader?limit=5000&name=fonts/[name].[ext]'}
     ]
     },
-    postcss: [
-        require('autoprefixer')
-    ],
     plugins: [
         new webpack.BannerPlugin("Copyright by maggie."),
+        new webpack.LoaderOptionsPlugin({
+            postcss: [
+                require('autoprefixer')
+            ],
+        }),
         new HtmlWebpackPlugin({
             template: __dirname + "/app/index.tmpl.html"
         }),
@@ -38,7 +41,6 @@ module.export = {
                 'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
             }
         }),
-        new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false
